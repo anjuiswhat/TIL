399d83d7ddb7cd91506fd252c835d44480f149de
Boanerges153 <minseong.yi@gmail.com>
2021-06-24 23:38:16 +0900

    Update R_study.R

---
+++ R/R_study/R_study.R
@@ -0,0 +1,56 @@
+print("Hello world!")
+
+help()
+help(print)
+??print
+
+
+x0 <- 10
+x0
+
+x1 <- c(10, 20, 30)
+x1
+
+x2 <- c(1:100)
+x2
+
+x3 <- seq(19,15)
+x3
+
+x4 <- seq(1, 10, by = 4)
+x4
+
+x5 <- c( "a", "b", "c")
+x5
+
+
+ex_vector1 <- c(-1,0,1)
+
+
+# 벡터의 변수의 속성 확인
+str(x2)
+str(x5)
+str(ex_vector1)
+
+# 벡터의 길이 확인
+length(x2)
+length(x5)
+length(ex_vector1)
+
+rm()
+
+rm(list = ls())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
