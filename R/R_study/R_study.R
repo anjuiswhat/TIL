bcab6f0b0d85e6e6912ea14af0174ef1e2e61420
Boanerges153 <minseong.yi@gmail.com>
2021-06-25 15:46:03 +0900

    Update R_study.R

--- R/R_study/R_study.R
+++ R/R_study/R_study.R
@@ -53,7 +53,8 @@ rm( list = ls() )
 
 
 # 행렬 생성
-# matrix( 변수명, nrow = 행 개수, ncol = 열 개수 )
+# 행렬 데이터는 단일형 data set (한가지 데이터 type만 사용 가능)
+matrix( 변수명, nrow = 행 개수, ncol = 열 개수 )
 
 x <- c(1, 2, 3, 4, 5, 6)
 matrix(x, nrow = 2, ncol = 3)
@@ -61,12 +62,31 @@ matrix(x, nrow = 3, ncol = 2)
 
 # 행렬 생성 시, 배치순서 변경: 'byrow = T' 옵션 추가
 x <- c(1, 2, 3, 4, 5, 6)
-matrix(x, nrow = 2, ncol = 3)
-matrix(x, nrow = 2, ncol = 3, byrow = T)
+matrix(x, nrow = 2, ncol = 3) # 1열의 모든행을 채우고 다음 열로...
+matrix(x, nrow = 2, ncol = 3, byrow = T) # 1행의 모든 행을 채우고 다음 행으로...
 
 
+# 배열 생성
+# 단일형 data set (한가지 데이터 type만 사용 가능)
+# 행렬을 n차원으로 확대한 구조 
+array( 변수명, dim = c(행 개수, 열 개수, 차원 수) )
+
+y <- c(1, 2, 3, 4, 5, 6)
+array(y, dim = c(2, 2, 3))
 
 
+# 리스트 생성
+# 1차원의 다중형 data set  (여러가지 data type을 동시에 포함 가능)
+list( data1, data2, data3, ...... )
+
+z <- list(c(1,2,3), 'hello')
+z
+str(z) # 리스트 z 속성 확인
+
+
+# 데이터 프레임 생성
+# 다중형 data set (여러가지 data type을 동시에 포함 가능)
+data.frame( 변수명1, 변수명2, ..., 변수명n )
 
 
 
