2c844433d0c0c8fcae71acebf3a5a63936b608b5
Boanerges153 <minseong.yi@gmail.com>
2021-06-28 11:19:08 +0900

    Create K-Digital.R

---
+++ R/K-Digital/K-Digital.R
@@ -0,0 +1,36 @@
+# 디버깅을 위한 4가지 함수
+print()
+sprintf()    # 포매팅을 해서 출력
+paste()
+cat()
+
+print('하이')
+
+?sprintf
+sprintf("number : %s", 1232)
+
+
+
+
+
+letters
+print(letters)
+print(LETTERS)
+month.abb
+month.name
+
+
+x <- 1:50
+y <- x^2
+mean(x)
+plot(x,y)
+cor(x,y)
+# 변수의 데이터 형 확인
+typeof()
+mode()
+
+
+typeof(x)
+
+
+
