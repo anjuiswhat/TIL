53253b5d46129ec9ceffbcb928df0975ddc38bcb
Boanerges153 <minseong.yi@gmail.com>
2021-07-04 03:35:46 +0900

    Update R_study.R

--- R/R_study/R_study.R
+++ R/R_study/R_study.R
@@ -1,468 +1,127 @@
-print("Hello world!")
 
-help()
-help(print)
-??print
 
 
-x0 <- 10
-x0
 
-x1 <- c(10, 20, 30)
-x1
 
-x2 <- c(1:100)
-x2
+split(iris, iris$Species)
 
-x3 <- seq(19,15)
-x3
+lapply(split(iris, iris$Species), mean)
 
-x4 <- seq(1, 10, by = 4)
-x4
 
-x5 <- c( "a", "b", "c")
-x5
+# iris의 종별 Sepal.Length의 평균
 
-
-ex_vector1 <- c(-1,0,1)
-
-
-# 벡터의 변수의 속성 확인
-str(x2)
-str(x5)
-str(ex_vector1)
-
-# 벡터의 길이 확인
-length(x2)
-length(x5)
-length(ex_vector1)
-
-
-# 저장되어 있는 변수들을 모두 출력
-ls()
-
-# 인수로 주어지는 오브젝트 삭제
-rm()
-
-# 현재 저장되어 있는 모든 변수를 삭제
-# rm() 으로 일일히 삭제해도 되지만 한꺼번에 지우고 싶을땐
-# rm() 과 ls()의 list 를 결합한다.
-rm( list = ls() )
-
-
-# 행렬 생성
-# 행렬 데이터는 단일형 data set (한가지 데이터 type만 사용 가능)
-matrix( 변수명, nrow = 행 개수, ncol = 열 개수 )
-
-x <- c(1, 2, 3, 4, 5, 6)
-matrix(x, nrow = 2, ncol = 3)
-matrix(x, nrow = 3, ncol = 2)
-
-# 행렬 생성 시, 배치순서 변경: 'byrow = T' 옵션 추가
-x <- c(1, 2, 3, 4, 5, 6)
-matrix(x, nrow = 2, ncol = 3) # 1열의 모든행을 채우고 다음 열로...
-matrix(x, nrow = 2, ncol = 3, byrow = T) # 1행의 모든 행을 채우고 다음 행으로...
-
-
-# 배열 생성
-# 단일형 data set (한가지 데이터 type만 사용 가능)
-# 행렬을 n차원으로 확대한 구조 
-array( 변수명, dim = c(행 개수, 열 개수, 차원 수) )
-
-y <- c(1, 2, 3, 4, 5, 6)
-array(y, dim = c(2, 2, 3))
-
-
-# 리스트 생성
-# 1차원의 다중형 data set  (여러가지 data type을 동시에 포함 가능)
-list( data1, data2, data3, ...... )
-
-z <- list(c(1,2,3), 'hello')
-z
-str(z) # 리스트 z 속성 확인
-
-
-# 데이터 프레임 생성
-# 다중형 data set (여러가지 data type을 동시에 포함 가능)
-data.frame( 변수명1, 변수명2, ..., 변수명n )
-
-# data.frame 구성하는 변수(컬럼) 생성
-ID <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-SEX <- c("F", "M", "F", "M", "M", "F", "F", "F", "M", "F")
-AGE <- c(50, 40, 28, 50, 27, 23, 56, 47, 20, 38)
-AREA <- c("서울", "경기", "제주", "서울", "서울", "서울", "경기", "서울", "인천", "경기")
-
-
-# 변수를 포함한 데이터 프레임 구조로 dataframe_ex 데이터 세트에 저장
-dataframe_ex <- data.frame(ID, SEX, AGE, AREA)
-
-# dataframe_ex 조회
-dataframe_ex 
-
-# 데이터프레임 속성 확인
-str(dataframe_ex) 
-
-# dataframe 으로 dataset 을 구성할 때는
-# 각 변수에 들어있는 관측치의 개수가 동일해야만 dataframe() 함수를 적용할 수 있다.
-# 예를 들어 위의 예제에서 다른 변수들은 data 개수가 10개인데 AGE 의 데이터가 8개라면 오류가 발생한다.
-
-ID <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-SEX <- c("F", "M", "F", "M", "M", "F", "F", "F", "M", "F")
-AGE <- c(50, 40, 28, 50, 27, 23, 56, 47)
-AREA <- c("서울", "경기", "제주", "서울", "서울", "서울", "경기", "서울", "인천", "경기")
-
-data.frame(ID, SEX, AGE, AREA)
-
-# 하지만 변수의 data 가 일부 부족한게 아니라 단 1개만 존재할 경우에는
-# 변수들간의 data 의 개수가 맞지 않더라도
-# 첫번째 data 1개가 다음 행들을 자동으로 채워서 dataframe 을 완성하게 된다.
-
-
-ID <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-SEX <- c("F", "M", "F", "M", "M", "F", "F", "F", "M", "F")
-AGE <- c(50)
-AREA <- c("서울", "경기", "제주", "서울", "서울", "서울", "경기", "서울", "인천", "경기")
-
-data.frame(ID, SEX, AGE, AREA)
-
-install.packages("readxl")
-library(readxl)
-
-ex <- read_excel("R_study/HelloR-master_(source)/Data/data_ex.xls")
-View(ex)
-
-
-class(iris)
-str(iris)
-
-levels(iris$Species)
-
-
-iris.df
-
-
-
-
-
-
-
-
-mean(iris$Sepal.Length)
-
-(iris$Sepal.Length$NA) <- mean(iris$Sepal.Length)
-
-iris$Sepal.Length <- ifelse( is.na(iris$Sepal.Length), mean(iris$Sepal.Length, na.rm=T),iris$Sepal.Length) 
-
-table(is.na(iris$Sepal.Length))
-
-
-sex <- factor("m", c("m", "f"))
-sex
-
-
-
-
-
-
-tmp.mat <- iris
-tmp.mat[1,1] <- NA
-pos <- tmp.mat$'Species' == 'setosa'
-tmp.mat$Sepal.Length[is.na(tmp.mat$Sepal.Length)] <- mean(tmp.mat$Sepal.Length[pos], na.rm=T)
-tmp.mat
-iris
-
-x <- c(1, 2, 3, 4, 5)
-x-3
-
-
-
-x <- c(1,3,4)
-names(x) <- c("kim", "seo", "park")
-x
-
-x['kim']
-
-
-c( 1, 2, 3, c(1, 2, 3) )
-
-
-
-
-d <- matrix(1:9, ncol=3)
-d
-
-apply(d, 1, sum)
-apply(d, 2, sum)
 
 
 
 head(iris)
-str(iris)
+
+split(iris$Sepal.Length, iris$Species)
 
 
+lapply(
+    split(iris$Sepal.Length, iris$Species),
+    mean
+)
 
-apply(iris[,1:4], 2, sum)
-colSums(iris[, 1:4])
+tapply(
+    iris$Sepal.Length,
+    iris$Species,
+    mean
+)
 
-
-(x <- list(a=1:3, b=4:6))
-
-
-
-lapply( 1:3, function(x){ x*2 } )
-
-
-result <- lapply( 1:3, function(x){ x*2 } )
-result
-result[1]
-
-
-
-
-( result <- lapply(1:3 , function (x) { x*2 }) )
-unlist(result)
-
-
-
-
-lapply(iris[,1:4], mean)
-
-
-
-colMeans(iris[, 1:4])
+subset(iris,
+       select = -c(Sepal.Length,Species)
+       )
 
 
 
 
 
+?subset()
+
+names(iris)
+
+iris[ , names(iris) %in% c('Sepal.Length', 'Species')]
 
 
+x <- data.frame(name=c("a", "b", "c"), math=c(1, 2, 3))
+y <- data.frame(name=c("c", "b", "a"), english=c(4, 5, 6))
 
-
-
-
-
-
-d <- matrix(1:9, ncol=3)
-
-
-r1 <- apply( iris[,1:4], 2, sum )
-str(r1)
-class(r1)
-
-
-
-
-d <- as.data.frame( 
-                    matrix(
-                            unlist( 
-                                    lapply(iris[, 1:4], mean)
-                                    ),
-                            ncol=4, byrow=TRUE)
-                    )
-
-
-
-
-d <- as.data.frame( matrix( unlist( lapply(iris[, 1:4], mean) ), ncol=4, byrow=TRUE ) )
-names(d) <- names(iris[,1:4])
-d
-
-
-
-x <- list( data.frame(name="foo", value=1), data.frame(name="bar", value=2) )
 x
-unlist(x)
-
-
-
-x <- list(data.frame(name="foo", v1=1, v2='a'), data.frame(name="bar", v1=2, v2='b'))
-x
-y <- unlist(x)
 y
 
-data.frame( do.call( cbind, lapply(iris[, 1:4], mean) ) )
 
-do.call( cbind, lapply(iris[, 1:4], mean) )
-x <- do.call( cbind, lapply(iris[, 1:4], mean) )
-is.matrix(x)
+merge(
+    x, y, 'name'
+)
 
 
-x <- list( data.frame(name="foo", value=1), data.frame(name="bar", value=2) )
 
-x <- list(data.frame(name="foo", value=1),
-          data.frame(name="bar", value=2))
-do.call(rbind, x)
+?merge()
 
 
-( x <- lapply(iris[, 1:4], mean) )
-class(x)
 
-( x <- sapply(iris[, 1:4], mean) )
-class(x)
-as.data.frame(x)
-as.data.frame(t(x))
+iris
 
+iris[1,1] = NA
 
-              
+head(iris)
 
-( y <- sapply(iris, class) )
-class(y)
 
-z <- sapply(iris[, 1:4], function(x) { x > 3 })
+median(iris$Sepal.Length, na.rm=T)
 
-z <- sapply( iris[, 1:4], function(x) { x > 3 } )
-head(z)
-class(z)
 
 
 
-y <- sapply(iris[, 1:4], function(x) { x > 3 })
-class(y)
-head(y)
 
-tapply(1:10, rep(1, 10), sum)
-tapply(1:10, 1:10 %% 2 == 1, sum)
+tapply(
+    iris$Sepal.Length,
+    iris$Species,
+    median, na.rm=T
+)
 
 
 
-tapply(iris$Sepal.Length, iris$Species, mean)
+within(
+    iris,
+    
+    )
+)
 
 
-m <- matrix(1:8,
-            ncol=2,
-            dimnames = list(c("spring", "summer", "fall", "winter"),
-                            c("male", "female")))
-m
-tapply(m, list(c(1,1,2,2,1,1,2,2),
-               c(1,1,1,1,2,2,2,2)), sum)
 
-( x <- list(name="foo", height=c(1, 3, 5)) )
-x[1]
-x[2]
 
 
 
-x$name
-x[[1]]
 
 
-( x <- list(name="foo",
-            height=c(1, 3, 5)) )
 
 
 
-list( a=list(val=c(1, 2, 3)),
-      b=list(val=c(1, 2, 3, 4)))
 
 
-matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow=2, byrow=TRUE)
 
-( x <- matrix(1:9, ncol=3) )
 
 
-dimnames(x) <- list(c("r1", "r2", "r3"),
-                    c("c1", "c2", "c3"))
-x
-dimnames(x)
 
 
 
-rownames(x) <- c("r1", "r2", "r3")
-x
-rownames(x)
 
 
-colnames(x) <- c("c1", "c2", "c3")
-x
-colnames(x)
 
 
 
 
-x <- matrix(1:9, ncol=3)
-x
 
-x[1:2,]
 
-x[c(1, 3), c(1, 3)]
 
-( x <- matrix(1:9, nrow = 3,
-              dimnames = list(c("r1", "r2", "r3"),
-                              c("c1", "c2", "c3"))) )
-x["r1", ]
 
 
-( x <- matrix(c(1:4), ncol=2) )
-solve(x)
-x %*% solve(x)
 
-( x <- matrix(1:4, ncol=2) )
-t(x)
 
 
-( x <- matrix(1:6, ncol=3) )
-dim(x)
-dim(x) <- c(3,2)
-dim(x)
 
-data.frame(...,
-           dfdf
-           )
 
 
-( x <- array(1:12, dim = c(2, 2, 3)) )
 
-x[, , 2]
 
-
-?data.frame
-( d <- data.frame(x=c(1, 2, 3, 4, 5),
-                  y=c(2, 4, 6, 8, 10),
-                  z=c('M', 'F', 'M', 'F', 'M'),
-                  stringsAsFactors = TRUE) )
-str(d)
-
-
-ID <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-SEX <- c("F", "M", "F", "M", "M", "F", "F", "F", "M", "F")
-AGE <- c(50)
-AREA <- c("서울", "경기", "제주", "서울", "서울", "서울", "경기", "서울", "인천", "경기")
-d <- data.frame(ID, SEX, AGE, AREA)
-
-View(d)
-
-( d <- data.frame(x=c(1, 2, 3, 4, 5),
-                  y=c(2, 4, 6, 8, 10),
-                  z=c('M', 'F', 'M', 'F', 'M'),
-                  stringsAsFactors = TRUE) )
-d$x <- 6:10
-d
-d$w <- c("A", "B", "C", "D", "E")
-d
-str(d)
-
-( d <- data.frame(x=c(1, 2, 3, 4, 5),
-                  y=c(2, 4, 6, 8, 10)) )
-d[1, ]
-?data.frame[]
-
-
-( d <- data.frame(x=c(1, 2, 3, 4, 5),
-                  y=c(2, 4, 6, 8, 10),
-                  z=c('M', 'F', 'M', 'F', 'M'),
-                  stringsAsFactors = TRUE) )
-d[1, ]
-d[1, 2]
-d[c(1,3), 2]
-d[-1, -2]
-d[ , c("x", "y")]
-d[ , c("x")]
-d[ , c("x"), drop=FALSE]
-
-
-( d <- data.frame(a=1:3,
-                  b=4:6,
-                  c=7:9) )
-d[ , names(d) %in% c("b", "c")]
-d[, !names(d) %in% c("a")]
